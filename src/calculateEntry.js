7ea34dfd57df12fe004ecc47fb3f69f71168564b
Jay Santos <jaysant15@gmail.com>
2022-03-16 14:24:26 -0300

    Iniciando do projeto. Lets go :rocket:

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -5,7 +5,8 @@ function countEntrants(entrants) {
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  // seu código aqui 
+  // 
 }
 
 module.exports = { calculateEntry, countEntrants };
