52605b4bcdad89378807931ec9a92e566fdecdd4
Jay Santos <jaysant15@gmail.com>
2022-04-04 00:44:24 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const arrSpecies = data.species;
+  const getSpecie = arrSpecies.filter((species) => ids.find((elemento) => species.id === elemento));
+  return getSpecie;
 }
 
 module.exports = getSpeciesByIds;
