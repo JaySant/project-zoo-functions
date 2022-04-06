f5a06415b5be21fa5ea0daf6fae29b8a6ffa9ff6
Jay Santos <jaysant15@gmail.com>
2022-04-06 01:18:41 -0300

    Requisito 5 - arquivo certo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
-}
+  const obj = {};
 
+  if (animal === undefined) {
+    data.species.forEach((item) => {
+      obj[item.name] = item.residents.length;
+    });
+    return obj;
+  }
+  if (animal.specie && animal.sex) {
+    const resident = data.species.find((item) => item.name === animal.specie);
+    return resident.residents.filter((element) => element.sex === animal.sex).length;
+  }
+  if (Object.keys(animal).includes('specie')) {
+    const resident = data.species.find((item) => item.name === animal.specie);
+    return resident.residents.length;
+  }
+}
 module.exports = countAnimals;
