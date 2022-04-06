b5cc4d940aab7db698eba0d4179aab2a3781531b
Jay Santos <jaysant15@gmail.com>
2022-04-06 14:01:37 -0300

    Requisito 10 e 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,55 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+function officeHour(dayWeek) {
+  if (dayWeek === 'Monday') {
+    return 'CLOSED';
+  }
+  const hoursAM = Object.values(data.hours[dayWeek])[0];
+  const hoursPM = Object.values(data.hours[dayWeek])[1];
+  return `Open from ${hoursAM}am until ${hoursPM}pm`;
 }
 
+function availability(dayWeek) {
+  if (dayWeek === 'Monday') {
+    return 'The zoo will be closed!';
+  }
+
+  const animals = data.species.filter((item) => item.availability.includes(dayWeek));
+  return animals.map((item) => item.name);
+}
+
+function noParam() {
+  const objHours = {};
+  Object.keys(data.hours).forEach((item) => {
+    objHours[item] = {
+      officeHour: officeHour(item),
+      exhibition: availability(item),
+    };
+  });
+  return objHours;
+}
+
+function animal(animalWeek) {
+  return data.species.find((item) => item.name.includes(animalWeek)).availability;
+}
+
+function day(dayWeek) {
+  const objHours = {};
+  objHours[dayWeek] = {
+    officeHour: officeHour(dayWeek),
+    exhibition: availability(dayWeek),
+  };
+  return objHours;
+}
+
+function getSchedule(scheduleTarget) {
+  // seu código aqui
+  if (data.species.some((item) => item.name === scheduleTarget)) {
+    return animal(scheduleTarget);
+  }
+  if (Object.keys(data.hours).includes(scheduleTarget)) {
+    return day(scheduleTarget);
+  }
+  return noParam();
+}
 module.exports = getSchedule;
